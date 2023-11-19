0fc3d1f15a9d288b8815631ea8153608a1ff3096
Malcolm Payao <mocpayao15@gmail.com>
2023-11-19 19:40:44 +0800

    DONE WITH REVIEW FRONT END

--- src/Pages/Reviewpage.js
+++ src/Pages/Reviewpage.js
@@ -1,13 +1,24 @@
 import NavBar from '../Components/NavBar.js';
 import SubNav from '../Components/SubNavBar.js';
-
+import North from '../Components/HPNorth.js';
+import South from '../Components/HPSouth.js';
+import { Link } from "react-router-dom"; 
 
 const Reviewpage = () => {
     
     return (  
         <div className="ReviewBody">
             <NavBar/>
+            <div className="page-header">
+                <div className="page-title">Review</div>
+            </div>
             <SubNav/>
+            <Link className='containerClick' to='/'>
+                <South/>
+            </Link>
+            <Link className='containerClick' to='/'>
+                <North/>
+            </Link>
         </div>
     );
 }
