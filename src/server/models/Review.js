3362911cffcef763d287afe31908c3ffa6d14cc7
vylramos <141283889+vylramos@users.noreply.github.com>
2023-11-23 08:47:54 +0800

    update review

--- src/server/models/Review.js
+++ src/server/models/Review.js
@@ -1,23 +1,45 @@
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema(
+  {
+    commenterName: {
+      type: String,
+      required: true,
+    },
+    commentText: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
 const ReviewSchema = new Schema({
-    Title:{
-    
-    },
-    Dest:{
+  Title: {
+    type: String,
+    required: true,
+  },
+  Dest: {
+    type: String,
+    required: true,
+  },
+  Body: {
+    type: String,
+    required: true,
+  },
+  Likes: {
+    type: Number,
+    default: 0,
+  },
+  Shares: {
+    type: Number,
+    default: 0,
+  },
+  Comments: [CommentSchema],
+}, { timestamps: true });
 
-    },
-    Body:{
-
-    },
-    Comments:{
-        
-    }
-    
-},{timestamps:true})
-
-
-const Review = mongoose.model('Reviews', ReviewSchema, 'REVIEWS')
+const Review = mongoose.model('Review', ReviewSchema, 'REVIEWS');
 
 module.exports = Review;
