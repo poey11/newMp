e2ec1ff4ceecdd75f496f36ddc0b7170b50bce5d
Malcolm Payao <mocpayao15@gmail.com>
2023-12-05 00:25:45 +0800

    added port

--- src/server/server.js
+++ src/server/server.js
@@ -11,12 +11,7 @@ const  app = express();
 
 
 app.use(cors());
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://newmp-poey11.onrender.com');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
+
 app.use(express.json())
 app.use((req, res, next)=> {
     console.log(req.path, req.method)
